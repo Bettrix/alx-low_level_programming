41ed8ee332f83785a99f4b8c4b089fae6749b331
Bettrix <bettbrian022@gmail.com>
2023-02-27 18:55:40 +0000

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,25 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * print_rev - prints a string, followed by a new line.
- * @s: input string to print.
- */
+ *  * print_rev - prints a string, followed by a new line.
+ *   * @*str: input string to print.
+ *    * @s: input string to print.
+ *     */
 void print_rev(char *s)
 {
-	int i;
+		int i;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-		i--;
-	while (s[i] != '\0')
-	{
-	_putchar(s[i--]);
-	}
-	_putchar('\n');
+		while(s[i] != '\0')
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+			i--;
+		while(s[i] != '\0')
+		while (s[i] != '\0')
+		{
+			putchar(s[i--]);
+			_putchar(s[i--]);
+		}
+			_putchar('\n');
 }
