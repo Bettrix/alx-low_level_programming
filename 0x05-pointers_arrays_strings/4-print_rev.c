c8c9889c691acd2ca3d21d298be6a94c7ac257d4
Bettrix <bettbrian022@gmail.com>
2023-02-27 19:23:30 +0000

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@ void print_rev(char *s)
 {
 		int i;
 
-			while (s[i] != '\0')
-					{
-								i++;
-									}
-				i--;
-					while (s[i] != '\0')
-							{
-										_putchar(s[i--]);
-											}
-						_putchar('\n');
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+			i--;
+		while (s[i] != '\0')
+		{
+			_putchar(s[i--]);
+		}
+			_putchar('\n');
 }
