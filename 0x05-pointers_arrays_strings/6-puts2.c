66f27ac118aad702231f5ef2d05f98b8caa272c5
Bettrix <bettbrian022@gmail.com>
2023-02-27 21:08:23 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,10 @@ void puts2(char *str)
 	int i;
 
 	while (str[i] != '\0')
-		{
+	{
 	if (i % 2 == 0)
-		_putchar(str[i]);
-		i++;
-		}
-		_putchar('\n');
+	_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
 }
