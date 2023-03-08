bc7b977dea6ed30100cd1ac63d7e82e17e4feba4
Bettrix <bettbrian022@gmail.com>
2023-03-08 17:02:02 +0000

    1-print_rev_recursion- task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
-/*
- * 1-print_rev_recursion.c- function that prints a string in reverse.
- *
- * Return: always void
- */
+/**
+ *  * _print_rev_recursion - puts a string to stdout in reverse
+ *   *
+ *    * @s: string to put to stdout
+ *     *
+ *      * Return: always void
+*/
 void _print_rev_recursion(char *s)
 {
-	if (!*s)/*if *s points to a null character*/
-	{
-		_putchar('\n');
+	if (!*s)/*if *s points to a null char*/
 		return;
-	}
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
