3ffef762c695f6f2305919302666c670bbaca975
Bettrix <bettbrian022@gmail.com>
2023-03-08 16:39:15 +0000

    0-puts_recursion.c- task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
-/*
-*  _puts_recursion - puts a string to stdout recursively
-*
-*  @s: the string to output to stdout
-*
-* Return: always void
-*/
+/**
+ *  * _puts_recursion - puts a string to stdout recursively
+ *   *
+ *    * @s: string to put to stdout
+ *     *
+ *      * Return: always void
+ *       */
 void _puts_recursion(char *s)
 {
-	if (!*s)/*if *s points to a null character*/
+	if (!*s) /* if s points to null char */
 	{
 		_putchar('\n');
 		return;
